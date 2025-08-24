f0526cd91459ac0d6061b8a6983c46e433e7a8b1
alex <exampe@gmail.com>
2025-08-24 20:54:10 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+console.log(1);
+
+// let num = 12;
+
+// console.log(num);
+
+const leftBorderWidth = 1;
+
+console.log(leftBorderWidth);
+
+
+
+leftBorderWidth = 10;
+console.log(leftBorderWidth);
+
+{
+
+    let result = 1;
+
+}
+
+console.log(result);
+
+let ind; 
+console.log(ind); //NaN - нема ящика, underfind - ящик пустий
+
+
