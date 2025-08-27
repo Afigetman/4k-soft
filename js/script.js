1bed6c4b5a062363629fa5ebf723028445e2bc4b
alex <exampe@gmail.com>
2025-08-27 20:19:06 +0300

    Изменение в файле script.js

--- js/script.js
+++ js/script.js
@@ -1,27 +1,45 @@
-console.log(1);
+// console.log(1);
 
-// let num = 12;
+// // let num = 12;
 
-// console.log(num);
+// // console.log(num);
 
-const leftBorderWidth = 1;
+// const leftBorderWidth = 1;
 
-console.log(leftBorderWidth);
-
-
-
-leftBorderWidth = 10;
-console.log(leftBorderWidth);
-
-{
-
-    let result = 1;
-
-}
-
-console.log(result);
-
-let ind; 
-console.log(ind); //NaN - нема ящика, underfind - ящик пустий
+// console.log(leftBorderWidth);
 
 
+
+// leftBorderWidth = 10;
+// console.log(leftBorderWidth);
+
+// {
+
+//     let result = 1;
+
+// }
+
+// console.log(result);
+
+// let ind; 
+// console.log(ind); //NaN - нема ящика, underfind - ящик пустий
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt("Один из последних просмотренных фильмов?", ""),
+      b = prompt("На сколько оцените его?", ""),
+      c = prompt("Один из последних просмотренных фильмов?", ""),
+      d = prompt("На сколько оцените его?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
